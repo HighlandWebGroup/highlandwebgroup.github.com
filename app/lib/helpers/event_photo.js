11e8374bb9103a7957e78f39fc372a1868e24243
Ewan McDougall <mrloop@mrloop.com>
2013-01-19 12:20:42 +0000

    add photo caption

--- app/lib/helpers/event_photo.js
+++ app/lib/helpers/event_photo.js
@@ -2,7 +2,7 @@ Ember.Handlebars.registerBoundHelper('event_photo', function(ev){
   var photos = this.get('controller.photos').filterProperty('photo_album.event_id',ev.get('id'));
   var html = "";
   photos.forEach(function(photo,index,enumerable){
-    html = html + "<img src=\""+photo.get('photo_link')+"\">"
+    html = html + "<img src=\""+photo.get('photo_link')+"\"><p>"+photo.get('caption')+"</p>"
   });
   return html.htmlSafe();
 });
